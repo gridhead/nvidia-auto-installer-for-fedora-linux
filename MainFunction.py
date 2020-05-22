f6acb56eeb2dc79fa1c7397c35551b6c4610221c
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-22 18:45:05 +0530

    Added reboot prompt on success

--- MainFunction.py
+++ MainFunction.py
@@ -14,6 +14,16 @@ def main():
             RepoInstaller.main()
             DriverInstaller.main()
             ForcingLoader.main()
+            print(Style.BRIGHT + Fore.CYAN + "[ ✔ ]" + " " + "DRIVER INSTALLATION SUCCESSFULLY COMPLETED!" + Style.RESET_ALL)
+            while True:
+                userpick = input(Style.BRIGHT + Fore.YELLOW + "[ ! ]" + Style.RESET_ALL + " " + Fore.WHITE + "Do you wish to reboot your system now? (Y/N) " + Style.RESET_ALL)
+                if userpick == "y" or userpick == "Y":
+                    print(Style.BRIGHT + Fore.YELLOW + "[ ! ]" + Style.RESET_ALL + " " + Fore.WHITE + "Rebooting now" + Style.RESET_ALL)
+                    os.system("sudo systemctl reboot")
+                elif userpick == "n" or userpick == "y":
+                    print(Style.BRIGHT + Fore.YELLOW + "[ ! ]" + Style.RESET_ALL + " " + Fore.WHITE + "You would need to reboot to load up installed drivers" + Style.RESET_ALL)
+                    print(Style.BRIGHT + Fore.RED + "[ ✘ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Leaving installer" + Style.RESET_ALL)
+                    exit()
         elif repofetc == -1:
             print(Style.BRIGHT + Fore.RED + "[ ✘ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Installation cannot proceed without RPMFusion NVIDIA repository!" + Style.RESET_ALL)
             print(Style.BRIGHT + Fore.RED + "[ ✘ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Leaving installer" + Style.RESET_ALL)
@@ -21,6 +31,16 @@ def main():
         else:
             DriverInstaller.main()
             ForcingLoader.main()
+            print(Style.BRIGHT + Fore.CYAN + "[ ✔ ]" + " " + "DRIVER INSTALLATION SUCCESSFULLY COMPLETED!" + Style.RESET_ALL)
+            while True:
+                userpick = input(Style.BRIGHT + Fore.YELLOW + "[ ! ]" + Style.RESET_ALL + " " + Fore.WHITE + "Do you wish to reboot your system now? (Y/N) " + Style.RESET_ALL)
+                if userpick == "y" or userpick == "Y":
+                    print(Style.BRIGHT + Fore.YELLOW + "[ ! ]" + Style.RESET_ALL + " " + Fore.WHITE + "Rebooting now" + Style.RESET_ALL)
+                    os.system("sudo systemctl reboot")
+                elif userpick == "n" or userpick == "y":
+                    print(Style.BRIGHT + Fore.YELLOW + "[ ! ]" + Style.RESET_ALL + " " + Fore.WHITE + "You would need to reboot to load up installed drivers" + Style.RESET_ALL)
+                    print(Style.BRIGHT + Fore.RED + "[ ✘ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Leaving installer" + Style.RESET_ALL)
+                    exit()
     else:
         print(Style.BRIGHT + Fore.RED + "[ ✘ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Installation was cancelled voluntarily!" + Style.RESET_ALL)
         print(Style.BRIGHT + Fore.RED + "[ ✘ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Leaving installer" + Style.RESET_ALL)
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A CLI tool which lets you install proprietary NVIDIA drivers in Fedora WS 32+
 The CLI look and feel is heavily inspired from `systemd` and `eopkg` prompts.
 
 ## Note
-* Requires secure boot to be turned off in UEFI systems
+* Requires secure boot to be turned off in UEFI systems.
 * Requires superuser access for repo addition and driver setup.
 * Active internet connection is required to download drivers.
 * The drivers are fetched from the RPMFusion repository.
