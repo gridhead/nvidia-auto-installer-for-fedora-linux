ada84b043c0b5723b830f733c0905c3ea1f8ef04
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-22 18:33:43 +0530

    Restored kernel readtime to 300 seconds

--- DriverInstaller.py
+++ DriverInstaller.py
@@ -8,7 +8,7 @@ def main():
     os.system("sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs")
     print(Style.BRIGHT + Fore.GREEN + "[ ✔ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Driver package installation completed!" + Style.RESET_ALL)
     print(Style.BRIGHT + Fore.YELLOW + "[ ! ]" + Style.RESET_ALL + " " + Fore.WHITE + "Commencing mandatory sleep for 5 minutes to load up kernel modules" + Style.RESET_ALL)
-    time.sleep(30)
+    time.sleep(300)
     print(Style.BRIGHT + Fore.GREEN + "[ ✔ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Modified kernel modules have been loaded up!" + Style.RESET_ALL)
 
 if __name__ == "__main__":
