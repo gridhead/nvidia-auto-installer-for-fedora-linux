5d236874b240affa570201d609bf1457d90bc808
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-22 16:50:18 +0530

    Added abort call handling

--- MainFunction.py
+++ MainFunction.py
@@ -11,4 +11,9 @@ def main():
     RPMFHandler.main()
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except BaseException:
+        print("")
+        print(Style.BRIGHT + Fore.YELLOW + "[ ! ]" + Style.RESET_ALL + " " + Fore.WHITE + "Abort call received or exception found - Leaving installer." + Style.RESET_ALL)
+        exit()
