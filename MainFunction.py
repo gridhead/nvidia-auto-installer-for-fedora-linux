933e8f1ae905d2fcc407cc5aa24ad50008167592
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-22 16:18:53 +0530

    Added main execution source

---
+++ MainFunction.py
@@ -0,0 +1,10 @@
+import os, HostDetection, SupportCheck, RPMFHandler, PackageCheck
+from colorama import init, Fore, Back, Style
+
+init()
+
+def main():
+    print(Style.BRIGHT + Fore.LIGHTCYAN_EX + "[ ! ] NVIDIA AUTOINSTALLER FOR FEDORA WORKSTATION" + Style.RESET_ALL)
+
+if __name__ == "__main__":
+    main()
