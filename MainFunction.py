dff597be60e42779e88abc7589004c5a6b8b3b36
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-22 18:00:46 +0530

    Linked repository addition and refresh update

--- MainFunction.py
+++ MainFunction.py
@@ -1,4 +1,4 @@
-import os, HostDetection, SupportCheck, RPMFHandler, PackageCheck
+import os, HostDetection, SupportCheck, RPMFHandler, PackageCheck, RepoInstaller
 from colorama import init, Fore, Back, Style
 
 init()
@@ -9,9 +9,15 @@ def main():
     SupportCheck.main()
     userinst = PackageCheck.main()
     if userinst == 1:
-        RPMFHandler.main()
+        repofetc = RPMFHandler.main()
+        if repofetc == 1:
+            RepoInstaller.main()
+        else:
+            print(Style.BRIGHT + Fore.RED + "[ ✘ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Installation cannot proceed without RPMFusion NVIDIA repository!" + Style.RESET_ALL)
+            print(Style.BRIGHT + Fore.RED + "[ ✘ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Leaving installer" + Style.RESET_ALL)
+            exit()
     else:
-        print(Style.BRIGHT + Fore.RED + "[ ✘ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Installation was cancelled" + Style.RESET_ALL)
+        print(Style.BRIGHT + Fore.RED + "[ ✘ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Installation was cancelled voluntarily!" + Style.RESET_ALL)
         print(Style.BRIGHT + Fore.RED + "[ ✘ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Leaving installer" + Style.RESET_ALL)
         exit()
 
