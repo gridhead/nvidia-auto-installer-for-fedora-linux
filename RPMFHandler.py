d1c2f6ae86bb4e86ccb25ef74a09917de0a33c01
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-22 16:25:23 +0530

    Added RPMFusion ping handler

---
+++ RPMFHandler.py
@@ -0,0 +1,17 @@
+import subprocess
+from colorama import init, Fore, Back, Style
+
+init()
+
+def main():
+    print(Style.BRIGHT + Fore.CYAN + "[ ✔ ]" + " " + "ATTEMPTING CONNECTION TO RPMFUSION..." + Style.RESET_ALL)
+    retndata = subprocess.getstatusoutput("ping -c 3 -W 3 rpmfusion.org")[0]
+    if retndata == 0:
+        print(Style.BRIGHT + Fore.GREEN + "[ ✔ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Connection to RPMFusion server was established!" + Style.RESET_ALL)
+    else:
+        print(Style.BRIGHT + Fore.RED + "[ ✘ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Connection to RPMFusion server could not be established!" + Style.RESET_ALL)
+        print(Style.BRIGHT + Fore.RED + "[ ✘ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Leaving installer with ERROR CODE - RPCN" + Style.RESET_ALL)
+        exit()
+
+if __name__ == "__main__":
+    main()
