bb707d50d4dd410aacf429e2d07c0f1bbd25967c
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-27 01:14:30 +0530

    Fixed class object import for main function

--- MainFunction.py
+++ MainFunction.py
@@ -4,6 +4,7 @@ from colorama import init, Fore, Style
 from ColoramaCalls import StatusDecorator
 
 init()
+DecoratorObject = StatusDecorator()
 
 def main():
     print(Style.BRIGHT + Fore.LIGHTCYAN_EX + "[ # ] NVIDIA AUTOINSTALLER FOR FEDORA WORKSTATION" + Style.RESET_ALL)
@@ -16,36 +17,36 @@ def main():
             RepoInstaller.main()
             DriverInstaller.main()
             ForcingLoader.main()
-            StatusDecorator.SectionHeader("DRIVER INSTALLATION SUCCESSFULLY COMPLETED")
+            DecoratorObject.SectionHeader("DRIVER INSTALLATION SUCCESSFULLY COMPLETED")
             while True:
                 userpick = input(Style.BRIGHT + Fore.YELLOW + "[ ! ]" + Style.RESET_ALL + " " + Fore.WHITE + "Do you wish to reboot your system now? (Y/N) " + Style.RESET_ALL)
                 if userpick == "y" or userpick == "Y":
-                    StatusDecorator.WarningMessage("Rebooting now")
+                    DecoratorObject.WarningMessage("Rebooting now")
                     os.system("sudo systemctl reboot")
                 elif userpick == "n" or userpick == "y":
-                    StatusDecorator.WarningMessage("You would need to reboot to load up installed drivers")
-                    StatusDecorator.FailureMessage("Leaving installer")
+                    DecoratorObject.WarningMessage("You would need to reboot to load up installed drivers")
+                    DecoratorObject.FailureMessage("Leaving installer")
                     sys.exit(0)
         elif repofetc == -1:
-            StatusDecorator.FailureMessage("Installation cannot proceed without RPMFusion NVIDIA repository!")
-            StatusDecorator.FailureMessage("Leaving installer")
+            DecoratorObject.FailureMessage("Installation cannot proceed without RPMFusion NVIDIA repository!")
+            DecoratorObject.FailureMessage("Leaving installer")
             sys.exit(0)
         else:
             DriverInstaller.main()
             ForcingLoader.main()
-            StatusDecorator.SectionHeader("DRIVER INSTALLATION SUCCESSFULLY COMPLETED")
+            DecoratorObject.SectionHeader("DRIVER INSTALLATION SUCCESSFULLY COMPLETED")
             while True:
                 userpick = input(Style.BRIGHT + Fore.YELLOW + "[ ! ]" + Style.RESET_ALL + " " + Fore.WHITE + "Do you wish to reboot your system now? (Y/N) " + Style.RESET_ALL)
                 if userpick == "y" or userpick == "Y":
-                    StatusDecorator.WarningMessage("Rebooting now")
+                    DecoratorObject.WarningMessage("Rebooting now")
                     os.system("sudo systemctl reboot")
                 elif userpick == "n" or userpick == "y":
-                    StatusDecorator.WarningMessage("You would need to reboot to load up installed drivers")
-                    StatusDecorator.FailureMessage("Leaving installer")
+                    DecoratorObject.WarningMessage("You would need to reboot to load up installed drivers")
+                    DecoratorObject.FailureMessage("Leaving installer")
                     sys.exit(0)
     else:
-        StatusDecorator.FailureMessage("Installation was cancelled voluntarily!")
-        StatusDecorator.FailureMessage("Leaving installer")
+        DecoratorObject.FailureMessage("Installation was cancelled voluntarily!")
+        DecoratorObject.FailureMessage("Leaving installer")
         sys.exit(0)
 
 if __name__ == "__main__":
