08eec373d89ff2849004650457f75d729665298e
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-27 01:16:26 +0530

    Fixed class object import for RPMFusion handler

--- RPMFHandler.py
+++ RPMFHandler.py
@@ -3,20 +3,21 @@ from colorama import init, Fore, Style
 from ColoramaCalls import StatusDecorator
 
 init()
+DecoratorObject = StatusDecorator()
 
 def main():
-    StatusDecorator.SectionHeader("ATTEMPTING CONNECTION TO RPMFUSION...")
+    DecoratorObject.SectionHeader("ATTEMPTING CONNECTION TO RPMFUSION...")
     retndata = subprocess.getstatusoutput("ping -c 3 -W 3 rpmfusion.org")[0]
     if retndata == 0:
-        StatusDecorator.SuccessMessage("Connection to RPMFusion server was estabilished")
+        DecoratorObject.SuccessMessage("Connection to RPMFusion server was estabilished")
         comand = "dnf repolist | grep 'rpmfusion-nonfree-nvidia-driver'"
         prompt = subprocess.Popen(comand, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = prompt.communicate()[0].decode("utf-8")
         if "rpmfusion-nonfree-nvidia-driver" in output:
-            StatusDecorator.SuccessMessage("RPMFusion repository for Proprietary NVIDIA Driver detected")
+            DecoratorObject.SuccessMessage("RPMFusion repository for Proprietary NVIDIA Driver detected")
             return 0
         else:
-            StatusDecorator.WarningMessage("RPMFusion repository for Proprietary NVIDIA Driver not detected")
+            DecoratorObject.WarningMessage("RPMFusion repository for Proprietary NVIDIA Driver not detected")
             while True:
                 userpick = input(Style.BRIGHT + Fore.YELLOW + "[ ! ]" + Style.RESET_ALL + " " + Fore.WHITE + "Do you wish to fetch packages from this repository? (Y/N) " + Style.RESET_ALL)
                 if userpick == "y" or userpick == "Y":
@@ -24,8 +25,8 @@ def main():
                 elif userpick == "n" or userpick == "N":
                     return -1
     else:
-        StatusDecorator.FailureMessage("Connection to RPMFusion server could not be established!")
-        StatusDecorator.FailureMessage("Leaving installer")
+        DecoratorObject.FailureMessage("Connection to RPMFusion server could not be established!")
+        DecoratorObject.FailureMessage("Leaving installer")
         sys.exit(0)
 
 if __name__ == "__main__":
