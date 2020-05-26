ebc51ff39a2e2c2fb6d6b40387508f5d042756c6
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-27 00:52:33 +0530

    Bound forcing loader with colorama calls

--- ForcingLoader.py
+++ ForcingLoader.py
@@ -1,18 +1,19 @@
-import os, time
+import os, sys, time
 from colorama import init, Fore, Style
+from ColoramaCalls import StatusDecorator
 
 init()
 
 def main():
-    print(Style.BRIGHT + Fore.CYAN + "[ ✔ ]" + " " + "FORCING READ FROM NEW KERNEL MODULES..." + Style.RESET_ALL)
+    StatusDecorator.SectionHeader("FORCING READ FROM NEW KERNEL MODULES...")
     os.system("sudo akmods --force")
-    print(Style.BRIGHT + Fore.GREEN + "     " + Style.RESET_ALL + " " + Fore.WHITE + "Read modified kernel module progressing [1/2]" + Style.RESET_ALL)
+    StatusDecorator.NormalMessage("Read modified kernel module progressing [1/2]")
     os.system("sudo dracut --force")
-    print(Style.BRIGHT + Fore.GREEN + "     " + Style.RESET_ALL + " " + Fore.WHITE + "Read modified kernel module progressing [2/2]" + Style.RESET_ALL)
-    print(Style.BRIGHT + Fore.GREEN + "[ ✔ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Kernel module installation completed!" + Style.RESET_ALL)
-    print(Style.BRIGHT + Fore.YELLOW + "[ ! ]" + Style.RESET_ALL + " " + Fore.WHITE + "Commencing mandatory sleep for 3 minutes to save kernel read config" + Style.RESET_ALL)
+    StatusDecorator.NormalMessage("Read modified kernel module progressing [2/2]")
+    StatusDecorator.SuccessMessage("Kernel module installation completed!")
+    StatusDecorator.WarningMessage("Commencing mandatory sleep for 3 minutes to save kernel read config")
     time.sleep(180)
-    print(Style.BRIGHT + Fore.GREEN + "[ ✔ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Kernel read configs have been stored!" + Style.RESET_ALL)
+    StatusDecorator.SuccessMessage("Kernel read configs have been stored!")
 
 if __name__ == "__main__":
     main()
