08b1872f3597a179702c937ba8802999319383fb
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-27 01:15:01 +0530

    Fixed class object import for forcing loader

--- ForcingLoader.py
+++ ForcingLoader.py
@@ -3,17 +3,18 @@ from colorama import init, Fore, Style
 from ColoramaCalls import StatusDecorator
 
 init()
+DecoratorObject = StatusDecorator()
 
 def main():
-    StatusDecorator.SectionHeader("FORCING READ FROM NEW KERNEL MODULES...")
+    DecoratorObject.SectionHeader("FORCING READ FROM NEW KERNEL MODULES...")
     os.system("sudo akmods --force")
-    StatusDecorator.NormalMessage("Read modified kernel module progressing [1/2]")
+    DecoratorObject.NormalMessage("Read modified kernel module progressing [1/2]")
     os.system("sudo dracut --force")
-    StatusDecorator.NormalMessage("Read modified kernel module progressing [2/2]")
-    StatusDecorator.SuccessMessage("Kernel module installation completed!")
-    StatusDecorator.WarningMessage("Commencing mandatory sleep for 3 minutes to save kernel read config")
+    DecoratorObject.NormalMessage("Read modified kernel module progressing [2/2]")
+    DecoratorObject.SuccessMessage("Kernel module installation completed!")
+    DecoratorObject.WarningMessage("Commencing mandatory sleep for 3 minutes to save kernel read config")
     time.sleep(180)
-    StatusDecorator.SuccessMessage("Kernel read configs have been stored!")
+    DecoratorObject.SuccessMessage("Kernel read configs have been stored!")
 
 if __name__ == "__main__":
     main()
