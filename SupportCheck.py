2238c4a0b90ec5a6af6f8ca96295a6c512cb813c
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-27 01:16:37 +0530

    Fixed class object import for support check

--- SupportCheck.py
+++ SupportCheck.py
@@ -3,28 +3,29 @@ from colorama import init
 from ColoramaCalls import StatusDecorator
 
 init()
+DecoratorObject = StatusDecorator()
 
 def main():
-    StatusDecorator.SectionHeader("CHECKING FOR GPU COMPATIBILITY...")
+    DecoratorObject.SectionHeader("CHECKING FOR GPU COMPATIBILITY...")
     comand = "lspci | grep -E 'VGA|3D'"
     prompt = subprocess.Popen(comand, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     output = prompt.communicate()[0].decode("utf-8")
     linect = output.count("\n")
     pkname = output.split("\n")
-    StatusDecorator.SuccessMessage("Compatibility infomation was obtained")
+    DecoratorObject.SuccessMessage("Compatibility infomation was obtained")
     for indx in pkname:
         if indx != "":
-            StatusDecorator.NormalMessage(indx)
+            DecoratorObject.NormalMessage(indx)
     if "NVIDIA" not in output:
-        StatusDecorator.FailureMessage("No supported NVIDIA GPU was detected!")
-        StatusDecorator.FailureMessage("Leaving installer with ERROR CODE - NVNF")
+        DecoratorObject.FailureMessage("No supported NVIDIA GPU was detected!")
+        DecoratorObject.FailureMessage("Leaving installer with ERROR CODE - NVNF")
         sys.exit(0)
     else:
-        StatusDecorator.SuccessMessage("An active NVIDIA GPU was detected!")
+        DecoratorObject.SuccessMessage("An active NVIDIA GPU was detected!")
     if linect == 1:
-        StatusDecorator.SuccessMessage("An single dedicated GPU setup was detected!")
+        DecoratorObject.SuccessMessage("An single dedicated GPU setup was detected!")
     else:
-        StatusDecorator.SuccessMessage("An Optimus Dual GPU setup was detected!")
+        DecoratorObject.SuccessMessage("An Optimus Dual GPU setup was detected!")
 
 if __name__ == "__main__":
     main()
