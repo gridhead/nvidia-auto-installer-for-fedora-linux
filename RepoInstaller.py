4b15580266d85e610fa2014baf993b7e96d5a672
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-27 00:53:22 +0530

    Bound repo installer with colorama calls

--- RepoInstaller.py
+++ RepoInstaller.py
@@ -1,18 +1,18 @@
 import subprocess, os, sys
-from colorama import init, Fore, Back, Style
+from ColoramaCalls import StatusDecorator
 
 init()
 
 def main():
-    print(Style.BRIGHT + Fore.CYAN + "[ ✔ ]" + " " + "FETCHING REPOSITORY DATA..." + Style.RESET_ALL)
+    StatusDecorator.SectionHeader("FETCHING REPOSITORY DATA...")
     retndata = subprocess.getstatusoutput("sudo dnf config-manager --set-enable rpmfusion-nonfree-nvidia-driver")[0]
     if retndata == 0:
-        print(Style.BRIGHT + Fore.GREEN + "[ ✔ ]" + Style.RESET_ALL + " " + Fore.WHITE + "RPMFusion NVIDIA repository was enabled!" + Style.RESET_ALL)
+        StatusDecorator.SuccessMessage("RPMFusion NVIDIA repository was enabled")
         os.system("sudo dnf update --refresh")
-        print(Style.BRIGHT + Fore.GREEN + "[ ✔ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Your packages have been updated!" + Style.RESET_ALL)
+        StatusDecorator.SuccessMessage("Your packages have been updated")
     else:
-        print(Style.BRIGHT + Fore.RED + "[ ✘ ]" + Style.RESET_ALL + " " + Fore.WHITE + "RPMFusion NVIDIA repository could not be enabled!" + Style.RESET_ALL)
-        print(Style.BRIGHT + Fore.RED + "[ ✘ ]" + Style.RESET_ALL + " " + Fore.WHITE + "Leaving installer" + Style.RESET_ALL)
+        StatusDecorator.FailureMessage("RPMFusion NVIDIA repository could not be enabled")
+        StatusDecorator.FailureMessage("Leaving installer")
         sys.exit(0)
 
 if __name__ == "__main__":
