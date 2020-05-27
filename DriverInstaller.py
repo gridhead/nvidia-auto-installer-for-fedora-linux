576e5e4871651727819db8186ceba93323e9dab3
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-27 11:02:50 +0530

    Resetted installation sleep time

--- DriverInstaller.py
+++ DriverInstaller.py
@@ -11,7 +11,7 @@ def main():
     if ExecStatusCode == 0:
         DecoratorObject.SuccessMessage("Driver package installation completed")
         DecoratorObject.WarningMessage("Commencing mandatory sleep for 5 minutes to load up kernel modules")
-        #time.sleep(300)
+        time.sleep(300)
         DecoratorObject.SuccessMessage("Modified kernel modules have been loaded up")
     else:
         DecoratorObject.FailureMessage("Could not install proprietary drivers")
