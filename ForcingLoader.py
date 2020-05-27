0fd0fcec73e29b9e8bd8593a37cfe508eb626997
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-27 10:39:30 +0530

    Added status code check for kernel module reader
    Enables checking if the force read from the modified kernel modules has really succeeded or not.

--- ForcingLoader.py
+++ ForcingLoader.py
@@ -7,14 +7,24 @@ DecoratorObject = StatusDecorator()
 
 def main():
     DecoratorObject.SectionHeader("FORCING READ FROM NEW KERNEL MODULES...")
-    os.system("sudo akmods --force")
     DecoratorObject.NormalMessage("Read modified kernel module progressing [1/2]")
-    os.system("sudo dracut --force")
-    DecoratorObject.NormalMessage("Read modified kernel module progressing [2/2]")
-    DecoratorObject.SuccessMessage("Kernel module installation completed!")
-    DecoratorObject.WarningMessage("Commencing mandatory sleep for 3 minutes to save kernel read config")
-    time.sleep(180)
-    DecoratorObject.SuccessMessage("Kernel read configs have been stored!")
+    ExecStatusCode01 = os.system("sudo akmods --force")
+    if ExecStatusCode01 == 0:
+        DecoratorObject.NormalMessage("Read modified kernel module progressing [2/2]")
+        ExecStatusCode02 = os.system("sudo dracut --force")
+        if ExecStatusCode02 == 0:
+            DecoratorObject.SuccessMessage("Kernel module installation completed!")
+            DecoratorObject.WarningMessage("Commencing mandatory sleep for 3 minutes to save kernel read config")
+            #time.sleep(180)
+            DecoratorObject.SuccessMessage("Kernel read configs have been stored!")
+        else:
+            DecoratorObject.FailureMessage("Reading modified kernel configs was terminated prematurely")
+            DecoratorObject.FailureMessage("Leaving installer")
+            sys.exit(0)
+    else:
+        DecoratorObject.FailureMessage("Reading modified kernel configs was terminated prematurely")
+        DecoratorObject.FailureMessage("Leaving installer")
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
