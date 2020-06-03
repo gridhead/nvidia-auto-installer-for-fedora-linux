3c53d1189f455733b5cc82c282cc99350fafe021
t0xic0der <akashdeep.dhar@gmail.com>
2020-06-04 00:57:28 +0530

    Integrated low-level functions in support check
    Removed view code and retained only the low level functions of GPU and host check

--- SupportCheck.py
+++ SupportCheck.py
@@ -1,31 +1,43 @@
-import subprocess, sys
-from colorama import init
-from ColoramaCalls import StatusDecorator
+import subprocess, os, distro
 
-init()
-DecoratorObject = StatusDecorator()
-
-def main():
-    DecoratorObject.SectionHeader("CHECKING FOR GPU COMPATIBILITY...")
+def gpuc():
     comand = "lspci | grep -E 'VGA|3D'"
     prompt = subprocess.Popen(comand, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     output = prompt.communicate()[0].decode("utf-8")
     linect = output.count("\n")
     pkname = output.split("\n")
-    DecoratorObject.SuccessMessage("Compatibility infomation was obtained")
-    for indx in pkname:
-        if indx != "":
-            DecoratorObject.NormalMessage(indx)
     if "NVIDIA" not in output:
-        DecoratorObject.FailureMessage("No supported NVIDIA GPU was detected!")
-        DecoratorObject.FailureMessage("Leaving installer with ERROR CODE - NVNF")
-        sys.exit(0)
+        return False
     else:
-        DecoratorObject.SuccessMessage("An active NVIDIA GPU was detected!")
-    if linect == 1:
-        DecoratorObject.SuccessMessage("An single dedicated GPU setup was detected!")
+        if linect == 1:
+            supprt = "single"
+        else:
+            supprt = "optims"
+        jsondt = {
+            "supprt": supprt,
+            "gpuqnt": linect,
+            "gpulst": pkname,
+        }
+        return jsondt
+
+def main():
+    jsondt = {
+        "System": str(os.uname().sysname) + " v" + str(os.uname().release),
+        "Hostname": str(os.uname().nodename),
+        "Version": str(os.uname().version),
+        "Distribution": str(distro.os_release_info()["name"]) + " " + str(
+            distro.os_release_info()["version_id"]) + " " + str(os.uname().machine),
+    }
+    return jsondt
+
+def avbl():
+    if str(distro.os_release_info()["name"]) == "Fedora":
+        if int(distro.os_release_info()["version_id"]) >= 32:
+            return "full"
+        else:
+            return "half"
     else:
-        DecoratorObject.SuccessMessage("An Optimus Dual GPU setup was detected!")
+        return False
 
 if __name__ == "__main__":
     main()
