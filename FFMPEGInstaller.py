fb4bf842b837814a36661ba417d69f0254e9b409
t0xic0der <akashdeep.dhar@gmail.com>
2020-06-04 15:23:27 +0530

    Removed dedicated FFMPEG support installer

--- FFMPEGInstaller.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def main():
-    ExecStatusCode = os.system("dnf install -y xorg-x11-drv-nvidia-cuda-libs")
-    if ExecStatusCode == 0:
-        return True
-    else:
-        return False
