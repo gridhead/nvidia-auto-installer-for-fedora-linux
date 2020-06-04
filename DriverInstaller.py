cfbf6d99a00d40925b5135442fef311326ee87f8
t0xic0der <akashdeep.dhar@gmail.com>
2020-06-04 07:54:41 +0530

    Added existing package check in driver installer
    Repurposed in x86 libraries and driver installer modules

--- DriverInstaller.py
+++ DriverInstaller.py
@@ -1,4 +1,4 @@
-import os
+import os, subprocess
 
 def main():
     ExecStatusCode = os.system("dnf install -y gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs")
@@ -6,6 +6,17 @@ def main():
         return True
     else:
         return False
+
+def avbl():
+    comand = "rpm -qa | grep 'nvidia'"
+    prompt = subprocess.Popen(comand, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = prompt.communicate()[0].decode("utf-8")
+    linect = output.count("\n")
+    if linect == 0:
+        return False
+    else:
+        pkname = output.split("\n")
+        return pkname
 
 if __name__ == "__main__":
     main()
