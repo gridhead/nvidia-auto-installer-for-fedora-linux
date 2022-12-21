704d57a198be80f602a1e25a4c94e63dac066d74
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-12-21 10:08:16 -0600

    Remove debugging print statements

--- nvautoinstall/operations/install_cublas_support.py
+++ nvautoinstall/operations/install_cublas_support.py
@@ -23,7 +23,6 @@ import subprocess
 
 class InstallCuBLASSupport(object):
     def search(self, version):
-        # print("TODO: INSTALL CUBLAS VERSION", version)
         command = "dnf search libcublas -q | grep -v devel"
         prompt = subprocess.Popen(
             command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
@@ -31,11 +30,9 @@ class InstallCuBLASSupport(object):
         output = prompt.communicate()[0].decode("utf-8")
         lines = output.splitlines()
         assert lines[0].startswith("====="), lines[0]
-        # print("dnf search:", lines)
         lines = lines[1:]
         packages = [l.split(" : ")[0] for l in lines]
         version_numbers = [p[p.index("-")+1:p.rindex(".")] for p in packages]
-        # print("versions:", version_numbers)
         if len(version_numbers) == 0:
             return version_numbers, None
         if version == "latest":
@@ -45,7 +42,6 @@ class InstallCuBLASSupport(object):
         if version_to_install is None:
             return version_numbers, None
         package_to_install = "libcublas-" + version_to_install
-        print("MATCHED PACKAGE", version_to_install, package_to_install)
         return version_numbers, package_to_install
 
     def main(self, package_name):
