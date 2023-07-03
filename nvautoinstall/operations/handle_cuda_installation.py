4b3d61c910e9bfdddd77ee3f88d884432a199446
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2023-07-03 08:20:16 +0530

    Upgrade the base Fedora Linux version for CUDA installation

--- nvautoinstall/operations/handle_cuda_installation.py
+++ nvautoinstall/operations/handle_cuda_installation.py
@@ -32,7 +32,7 @@ class HandleCudaInstallation:
 
     def rpin(self):
         retndata = subprocess.getstatusoutput(
-            "dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/fedora35/x86_64/cuda-fedora35.repo"  # noqa
+            "dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/fedora37/x86_64/cuda-fedora37.repo"  # noqa
         )[0]
         return retndata == 0
 
